2a3fd7a0288955785d696e2d3a9c2f96ef697739
phrazzld <phrazzld@pm.me>
2025-05-07 08:57:16 +0900

    test: pre-commit hook with fixed type

---
+++ test-type-error.ts
@@ -0,0 +1,7 @@
+// This file is for testing the pre-commit hook
+// It contains a fixed type error
+
+// Fixed type: number assigned to number
+const testNumber: number = 42;
+
+export default testNumber;
